cf5f4b1c38bbbf5c9454b3e991f86bde3760878b
Peter J. Kohler, PhD <pjkohl3r@gmail.com>
2017-06-02 11:07:47 -0700

    updated to better handle paired tests

--- tools/fitting/fitErrorEllipse/tSquaredFourierCoefs.m
+++ tools/fitting/fitErrorEllipse/tSquaredFourierCoefs.m
@@ -1,84 +1,96 @@
-function [results] = tSquaredFourierCoefs(xyData1,xyData2,testMu,alphaVal,pdStyle)
-% [results] = tSquaredFourierCoefs(xyData,testMu,alphaVal)
-%
-% Returns the results of running Hotelling's t-squared test that the mean
-% of the 2D data in xyData is the same as the mean specified in testMu at
-% the specified alphaVal (0-1).
-%
-% results contains the following fields:
-%   alpha, tSqrdCritical, tSqrd, pVal, H (0 if can't reject null; 1 if
-%       rejected the null hypothesis)
-%
-% If testMu is not provided, the mean is tested against the origin (0,0).
-% If alphaVal is not provided, it defaults to 0.05.
-%
-% This function assumes that the 2D data in xyData are Fourier coefficients
-% organized with the real coefficients in the 1st column and the imaginary
-% coefficients in the 2nd column. Rows = samples.
-%
-% Based on Anderson (1984) An Introduction to Multivariate Statistical 
-% Analysis, Wiley
+function [results] = tSquaredFourierCoefs(xyData,varargin)
+    % Syntax: [results] = tSquaredFourierCoefs(xyData,testMu,alphaVal)
+    %
+    % Returns the results of running Hotelling's t-squared test that the mean
+    % of the 2D data in xyData is the same as the mean specified in testMu at
+    % the specified alphaVal (0-1).
+    %
+    % Based on Anderson (1984) An Introduction to Multivariate Statistical 
+    % Analysis, Wiley
+    %
+    % In:
+    %   xyData - n x 2 x q matrix containing n data samples
+    %            if q = 1, data will be tested against zero    
+    %            if q = 2, a paired t-test will be performed,
+    %            testing xyData(:,:,1) against xyData(:,:,2). 
+    %            2 is the maximum length of the third dimension. 
+    %            Function assumes that the 2D data in xyData(:,:,1)
+    %            and the optional xyData(:,:,2) are Fourier 
+    %            coefficients organized with the real coefficients 
+    %            in the 1st column and the imaginary
+    %            coefficients in the 2nd column. Rows = samples.
+    % 
+    % <options>:
+    %   testMu - 2-element vector specifying the mean to test against ([0,0]) 
+    % 
+    %   alphaVal - scalar indicating the alpha value for testing ([0.05])
+    %
+    %   pdStyle - do PowerDiva style testing (true/[false])
+    %
+    % Out:
+    %
+    %   results - struct containing the following fields:
+    %             alpha, tSqrdCritical, tSqrd, pVal, H (0 if can't reject null; 1 if
+    %             rejected the null hypothesis)
 
-if nargin<5 || isempty(alphaVal)
-    pdStyle = false;
-end
+    opt	= ParseArgsOpt(varargin,...
+        'testMu'		, [0,0], ...
+        'alphaVal'		, 0.05	, ...
+        'pdStyle',        true ...
+        );
 
-if nargin<4 || isempty(alphaVal)
-    alphaVal = 0.05;
-end
-if nargin<3 || isempty(testMu)
-    testMu = [0,0];
-else
-end
-if nargin<2 || isempty(testMu)
-    xyData2 = zeros(size(xyData1));
-else
-end
-
-dims = size(xyData1);
-N = dims(1);
-if dims(2) ~= 2
-    error('input data must be a matrix of 2D row samples');
-end
-if N < 2
-    error('input data must contain at least 2 samples');
-end
-
-if length(testMu) ~= 2
-    error('testMu should be a 2D vector');
-end
-
-xyData = xyData1 - xyData2;
-
-try
-    [sampMu,sampCovMat] = eigFourierCoefs(xyData);
-catch
-    fprintf('The covariance matrix of xyData could not be calculated, probably your data do not contain >1 sample.');
-end
-
-if pdStyle
-    sampCovMat = eye(2);
-else
-end
-
-results.alpha = alphaVal;
-
-% Eqn. 2 in Sec. 5.3 of Anderson (1984):
-t0Sqrd = ((N-1)*2)/(N-2) * finv( 1-alphaVal, 2, N - 2 ); 
-results.tSqrdCritical = t0Sqrd;
-
-try
-    invSampCovMat = inv(sampCovMat);    
-    % Eqn. 2 of Sec. 5.1 of Anderson (1984):
-    tSqrd = N * (sampMu - testMu) * invSampCovMat * (sampMu - testMu)'; 
+    dims = size(xyData);
+    N = dims(1);
+    if dims(2) ~= 2
+        error('input data must be a matrix of 2D row samples');
+    end
+    if N < 2
+        error('input data must contain at least 2 samples');
+    end
     
-    tSqrdF = (N-2)/((N-1)*2) * tSqrd; % F approximation 
-    pVal = 1 - fcdf(tSqrdF, 2, N-2);  % compute p-value
-    
-    results.tSqrd = tSqrd;
-    results.pVal = pVal;
-    results.H = tSqrd >= t0Sqrd;
-catch
-    fprintf('inverse of the sample covariance matrix could not be computed.')
+    if length(dims) < 3 % if no third dimension
+        xyData(:,:,2) = zeros(size(xyData));
+    elseif dims(3) > 2
+         error('length of third dimension of input data may not exceed two')
+    else
+    end
+
+    if length(opt.testMu) ~= 2
+        error('testMu should be a 2D vector');
+    end
+
+    xyData = xyData(:,:,1) - xyData(:,:,2);
+
+    try
+        [sampMu,sampCovMat] = eigFourierCoefs(xyData);
+    catch
+        fprintf('The covariance matrix of xyData could not be calculated, probably your data do not contain >1 sample.');
+    end
+
+    if opt.pdStyle
+        sampCovMat = eye(2);
+    else
+    end
+
+    results.alpha = opt.alphaVal;
+
+    % Eqn. 2 in Sec. 5.3 of Anderson (1984):
+    t0Sqrd = ((N-1)*2)/(N-2) * finv( 1-opt.alphaVal, 2, N - 2 ); 
+    results.tSqrdCritical = t0Sqrd;
+
+    try
+        invSampCovMat = inv(sampCovMat);    
+        % Eqn. 2 of Sec. 5.1 of Anderson (1984):
+        tSqrd = N * (sampMu - opt.testMu) * invSampCovMat * (sampMu - opt.testMu)'; 
+
+        tSqrdF = (N-2)/((N-1)*2) * tSqrd; % F approximation 
+        pVal = 1 - fcdf(tSqrdF, 2, N-2);  % compute p-value
+
+        results.tSqrd = tSqrd;
+        results.pVal = pVal;
+        results.H = tSqrd >= t0Sqrd;
+    catch
+        fprintf('inverse of the sample covariance matrix could not be computed.')
+    end
 end
 
