b635bb0394c4cc4ab5e119c8c7ab6695dd8abc4e
babylab <babylab@sr13-62109148bc.stanford.edu>
2017-07-22 11:41:37 -0700

    lots of additional functionality

--- tools/visualization/steadyStateWaveMovie.m
+++ tools/visualization/steadyStateWaveMovie.m
@@ -35,7 +35,12 @@ function steadyStateWaveMovie(varargin)
             'colors', [], ...
             'slowdown', 10, ...
             'sampleRate', 20, ...
+            'movieDur',[], ...
+            'makeMovie',true, ...
             'outFormat', 'gif', ...
+            'figAspectRatio',  [4,2], ...
+            'phaseDeg',  0, ...
+            'printSingleWaves', false, ...
             'filename',  '~/Desktop/test' ...
             );
     
@@ -45,14 +50,26 @@ function steadyStateWaveMovie(varargin)
     else
     end
     
-    % movie duration in seconds, 
-    % x cycles of smallest frequency multiplied by the slowdown factor
-    movieDur = (1:10)./min(opt.freq);
-    % at least one second long
-    movieDur = min(movieDur(movieDur>1));
+    if isempty(opt.movieDur)
+        % movie duration in seconds, 
+        % x cycles of smallest frequency multiplied by the slowdown factor
+        opt.movieDur = (1:10)./min(opt.freq);
+        % at least one second long
+        opt.movieDur = min(opt.movieDur(opt.movieDur>=1));
+    else
+    end
+    
+%     if isempty(opt.slowdown)
+%         if opt.makeMovie
+%             opt.slowdown = 10;
+%         else
+%             opt.slowdown = 1;
+%         end
+%     else
+%     end
     
     % total number of samples
-    numSamples = movieDur * opt.slowdown * opt.sampleRate;  
+    numSamples = opt.movieDur * opt.slowdown * opt.sampleRate;  
     
     if ~ismember(opt.outFormat,{'gif','avi'})
         error('unknown format %s',opt.outFormat);
@@ -68,31 +85,34 @@ function steadyStateWaveMovie(varargin)
     % compute the sine waves
     numFreq = length(opt.freq);
     amp = 1;
-    deltaT = movieDur/numSamples;
-    T = 0:deltaT:movieDur;
+    T = linspace(0,opt.movieDur,numSamples+1);
     T = T(1:numSamples);
     sinFxn = zeros(numSamples,numFreq);
+    if numel(opt.phaseDeg) < numFreq
+        opt.phaseDeg = repmat(opt.phaseDeg(1),1,numFreq);
+    else
+    end
     for f = 1:numFreq
         circFreq = 2*pi*opt.freq(f);
-        sinFxn(:,f) = amp.*sin(circFreq.*T);
+        sinFxn(:,f) = amp.*sin( circFreq.*T + opt.phaseDeg(f) * pi/180);
     end
     
     % draw it
-    lWidth = 6;
+    lWidth = 10;
     gcaOpts = {'tickdir','out','ticklength',[0.0,0.0],'box','off','linewidth',lWidth};
 
     for f = 1:numFreq
         sinFig = figure;
         figPos = get(sinFig,'pos');
-        figPos(4) = figPos(4)*1.5;
-        figPos(3) = figPos(4)*3;
+        figPos(3) = figPos(4)*opt.figAspectRatio(1);
+        figPos(4) = figPos(4)*opt.figAspectRatio(2);
         set(sinFig,'pos',figPos);
         hold on
         for z = 1:numSamples
             plotH = plot(T(1:z),sinFxn(1:z,f),'color','k','linewidth',lWidth);
             set(gca,gcaOpts{:},'xtick',[],'ytick',[], 'visible', 'off','Color',[1 1 1]);
             warning('off','all')
-            xlim([-.05,movieDur+.05]);
+            xlim([-.05,opt.movieDur+.05]);
             ylim([-1.05,1.05]);
             drawnow
             pause(.1);
@@ -112,34 +132,64 @@ function steadyStateWaveMovie(varargin)
         
     movieMap(1,:) = [1 1 1]; % make background white (will be transparent in gif) 
     movieMap(2:numFreq+1,:) = opt.colors(1:numFreq,:);
-    movieFrames = squeeze(max(movieFrames,[],3));
+    movieFrames(:,:,end+1,:) = squeeze(max(movieFrames,[],3));
+   
+    % crop images
+    lastFrame = movieFrames(:,:,end,end);
+    colIdx = find(sum(lastFrame == 0,1) ~= size(lastFrame,1));
+    rowIdx = find(sum(lastFrame == 0,2) ~= size(lastFrame,2));    
+    movieFrames = movieFrames(rowIdx,colIdx,:,:);
     
-    imwrite(movieFrames(:,:,end),movieMap,[opt.filename,'_lastframe.gif'],'transparentColor',0);
-    if strcmp(opt.outFormat,'gif')
-        if exist([opt.filename,'gif'],'file')
-            delete([opt.filename,'gif']);
-        end
-        for k = 1:numSamples
-            if k==1
-                gifOpts = {'LoopCount',Inf};
+    for n = 1:(numFreq+1)
+        if n == (numFreq+1)
+            curFilename = opt.filename;
+            % always print full set of waveforms
+            printWave = true;
+        else
+            curFilename = sprintf('%s_%0.3fHz',opt.filename,opt.freq(n));
+            if opt.printSingleWaves
+                printWave = true;
             else
-                gifOpts = {'WriteMode','append'};
+                printWave = false;
             end
-            imwrite(movieFrames(:,:,k),movieMap,[opt.filename,'.gif'],'gif','DelayTime',1/opt.sampleRate,gifOpts{:},'transparentColor',0);
         end
-    else
-        if exist([opt.filename,'avi'],'file')
-            delete([opt.filename,'avi']);
+        if printWave
+            % write last frame as single image
+            if exist([curFilename,'_lastframe.gif'],'file')
+                delete([curFilename,'gif']);
+            end
+            imwrite(movieFrames(:,:,n,end),movieMap,[curFilename,'_lastframe.gif'],'transparentColor',0);
+            if opt.makeMovie
+                if strcmp(opt.outFormat,'gif')
+                    if exist([curFilename,'gif'],'file')
+                        delete([curFilename,'gif']);
+                    end
+                    for k = 1:numSamples
+                        if k==1
+                            gifOpts = {'LoopCount',Inf};
+                        else
+                            gifOpts = {'WriteMode','append'};
+                        end
+                        imwrite(movieFrames(:,:,n,k),movieMap,[curFilename,'.gif'],'gif','DelayTime',1/opt.sampleRate,gifOpts{:},'transparentColor',0);
+                    end
+                else
+                    if exist([opt.filename,'avi'],'file')
+                        delete([curFilename,'avi']);
+                    end
+                    vidObj = VideoWriter([curFilename,'.avi'],'Indexed AVI');
+                    vidObj.FrameRate = opt.sampleRate; % frames per second
+                    vidObj.Colormap = movieMap;
+                    open(vidObj);
+                    for k = 1:size(movieFrames, 4)
+                       % Write each frame to the file.
+                       writeVideo(vidObj,movieFrames(:,:,n,k));
+                    end
+                    close(vidObj);
+                end
+            else
+            end
+        else
         end
-        vidObj = VideoWriter([opt.filename,'.avi'],'Indexed AVI');
-        vidObj.FrameRate = opt.sampleRate; % frames per second
-        vidObj.Colormap = movieMap;
-        open(vidObj);
-        for k = 1:size(movieFrames, 3)
-           % Write each frame to the file.
-           writeVideo(vidObj,movieFrames(:,:,k));
-        end
-        close(vidObj);
     end
     warning('on','all')
 end
