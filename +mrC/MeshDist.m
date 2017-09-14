0a8715b35350487b26d338e9b9cb48f17d844c5f
Peter J. Kohler, PhD <pjkohl3r@gmail.com>
2017-09-14 12:20:19 -0700

    new MeshDist fxn (wholebrain+ROI), integrated into the RoiCorrelation fxn used for FACE

--- +mrC/MeshDist.m
+++ +mrC/MeshDist.m
@@ -1,44 +1,282 @@
-function [dist,coords] = MeshDist(subId,varargin)
-    % Description:	Get distances between mesh indices
-    % 
-    % Syntax:	mrC.MeshDist(subId,varargin)
-    % In:
-    %   subId - subject ID, without '_fs4' suffix (if given, it will be
-    %           removed
-    %
-    %   <options>:
-    %       
-    % Out:
-    
-    mrC.SetPrefs; % make sure preferences are set
-    
-    %% DETERMINE FREESURFER DIR
-    if strfind(subId,'_fs4');
-        subId = subId(1:end-4); % get rid of FS suffix, if given by user
-    else
-    end
-    fsDir = fullfile(getpref('freesurfer','SUBJECTS_DIR'),[subId,'_fs4']);
-    anatDir = fullfile(getpref('mrCurrent','AnatomyFolder'),subId);
+function [dist,idx] = MeshDist(subId,varargin)
+    % UNTITLED Summary of this function goes here
+    % Detailed explanation goes here
     
     %% SET DEFAULTS
-     opt	= ParseArgs(varargin,...
-             'CortexFile' ,fullfile(anatDir,'/Standard/meshes/defaultCortex.mat') ...
-             );
-         
-    %% LOAD MRMESH CORTEX AND TRANSFORM TO MATCH FREESURFER
-    load(opt.CortexFile);
-    msh.nVertex = sum(msh.nVertexLR);
-    % sanity check
-    if any( [ size(msh.data.vertices,2), size(msh.initVertices,2) ] ~= msh.nVertex )
-        error('vertex disagreement within %s',opt.CortexFile)
+    opt	= ParseArgs(varargin,...
+            'ROI',              'wholebrain', ...
+            'hemi',             'both',...
+            'measure',         'Geodesic', ...
+            'maxDist',          30 ...
+            );
+    %% SET UP SYSTEM AND GET FILES
+    mrC.SystemSetup;
+    % get directories
+    anatDir = fullfile(getpref('mrCurrent','AnatomyFolder'),subId);
+    meshDir = fullfile( anatDir, 'Standard', 'meshes' );
+    if ~isdir( meshDir )
+        error( 'Subject %s has no /Standard/meshes directory.', subId )
+    end
+    % load cortices 
+    subCortex = load( fullfile( meshDir, 'defaultCortex.mat' ) );
+    subCortex = subCortex.msh;
+    % get hemisphere indices
+    hIdx{1} = 1:subCortex.nVertexLR(1); % left idx
+    hIdx{2} = (subCortex.nVertexLR(1)+1):sum(subCortex.nVertexLR); % right idx
+    
+    %% GET ROIs
+    if strcmp(opt.ROI,'wholebrain');
+        roiIdx = hIdx; % all vertices
+        roiHemi = opt.hemi;
+    elseif exist(opt.ROI, 'file') || exist(sprintf('%s.mat',opt.ROI),'file')
+        % if ROI is a file name
+        [~,roiName,roiExt] = fileparts(opt.ROI);
+        tmpSplit = split_string(roiName,'-');
+        tmp = load(opt.ROI);
+        if strcmp(tmpSplit(end),'L');
+            roiHemi = 'left';
+            roiIdx{1} = tmp.ROI.meshIndices;
+        elseif strcmp(tmpSplit(end),'R');
+            roiHemi = 'right';
+            roiIdx{2} = tmp.ROI.meshIndices;
+        else
+            roiHemi = opt.hemi;
+            msg = sprintf( '\n Hemisphere could not be decoded, from ROI %s \n', roiName );
+            error(msg);
+        end
+        clear tmpSplit;
+        clear tmp;
+    else
+        % try to decode ROI type and name, assuming TYPE_NAME-HEMI convention
+        tmpSplit = split_string(opt.ROI,'_',2);
+        roiType = split_string(opt.ROI,'_',1);
+        tmpSplit = split_string(opt.ROI,'_',2,'-');
+        roiName = tmpSplit{1};
+        if numel(tmpSplit) == 1
+            roiHemi = 'both';
+        else
+            if strcmp(tmpSplit{2},'L');
+                roiHemi = 'left';
+            elseif strcmp(tmpSplit{2},'R');
+                roiHemi = 'right';
+            else
+                msg = sprintf( '\n Unknown hemisphere %s for ROI %s \n', roiHemi,opt.ROI );
+                error(msg);
+            end
+        end
+        switch roiType
+            case{ 'wang', 'wangatlas' }
+                % if wang ROI
+                roiList = {'V1v' 'V1d' 'V2v' 'V2d' 'V3v' 'V3d' 'hV4' 'VO1' 'VO2' 'PHC1' 'PHC2' ...
+                       'TO2' 'TO1' 'LO2' 'LO1' 'V3B' 'V3A' 'IPS0' 'IPS1' 'IPS2' 'IPS3' 'IPS4' ...
+                       'IPS5' 'SPL1' 'FEF'};
+                if strcmp(roiName,'V4')
+                    roiName = 'hV4';
+                else
+                end
+                if ismember(roiName,roiList)
+                    roiDir = subfolders( fullfile( meshDir, '*wang*'),1);
+                    roiDir = roiDir{1};
+                    roiPath = subfiles( sprintf('%s/wangatlas_%s*',roiDir,roiName) , 1);
+                    if roiPath{1} == 0
+                        % try just using 'wang' suffix
+                        roiPath = subfiles( sprintf('%s/wang_%s*',roiDir,roiName) , 1);
+                    else
+                    end
+                else
+                    roiPath{1} = 0;
+                end
+            case{ 'func', 'functional' }
+                % if functional ROI
+                roiList = {'V1d','V1v','V2v','V2d','V3v','V3d','V4','V3ab','LOC','MT','IPS0','VO1'};
+                if ismember(roiName,roiList)
+                    roiDir = fullfile( meshDir, 'ROIs');
+                    roiPath = subfiles( sprintf('%s/func_%s*',roiDir,roiName) , 1);
+                else
+                    roiPath{1} = 0;
+                end
+            otherwise
+                roiPath{1} = 0;
+        end
+        % roiPath should be a two-element (left and right) cell, with paths
+        if roiPath{1} == 0 
+            msg = sprintf( '\n ROI %s could not be found! \n', opt.ROI );
+            error(msg);
+        elseif numel(roiPath) > 2
+            msg = sprintf( '\n ROI %s lead to multiple ROI paths, check the files in %s! \n', opt.ROI, roiDir );
+            error(msg);
+        else
+            tmp = load(roiPath{1});
+            roiIdx{1} = tmp.ROI.meshIndices; % left
+            tmp = load(roiPath{2});
+            roiIdx{2} = tmp.ROI.meshIndices; % right
+        end
     end
     
-    kL = 1:msh.nVertexLR(1);
-    kR = (msh.nVertexLR(1)+1):msh.nVertex;
-    coords.all = msh.data.vertices';
-    coords.left = coords.all(kL,:);
-    coords.right = coords.all(kR,:);
-    dist.all = squareform(pdist(coords.all));
-    dist.left = squareform(pdist(coords.left));
-    dist.right = squareform(pdist(coords.right));
-end 
+    % if ROI contains information about hemisphere, assume that this is to
+    % be used
+    if ~strcmp(roiHemi,opt.hemi)
+        msg = sprintf('\n ROI %s%s is in %s hemisphere, changing hemi from ''%s'' to ''%s''\n',...
+        roiName,roiExt, roiHemi,opt.hemi,roiHemi);
+        warning(msg);
+        opt.hemi = roiHemi;
+    else
+    end
+    switch opt.hemi
+        case 'left'
+            hemiIdx = 1;
+        case 'right'
+            hemiIdx = 2;
+        case 'both'
+            hemiIdx = 3;
+    end
+    
+    %% COMPUTE DISTANCES
+    if hemiIdx == 3
+        curIdx = cat(2,roiIdx{1},roiIdx{2});   
+    else
+        curIdx = roiIdx{hemiIdx};
+    end
+    if strcmp(opt.measure,'Geodesic')
+        barString = {'left','right','both'};
+        if ~exist('vConn','var')
+            % only do this the first time
+            % triangles are indices starting at zero, so add one
+            [c, vConn] = ...
+                tess_vertices_connectivity( struct( 'faces',subCortex.data.triangles' + 1, 'vertices',subCortex.data.vertices' ) );
+            nVert = numel(vConn);
+        else
+        end
+        nInd = numel( curIdx );
+        distMat = 0.5 * eye( nInd );      % Initialization of the distance matrix
+
+        for iInd = 1:(nInd-1) % loop over all ROI vertices except one
+            if nInd > 1000
+                if ~exist('waitH','var')
+                    waitH = waitbar(0,sprintf('Computing distances for %s ...',barString{hemiIdx}));
+                else
+                end
+                waitbar(iInd/(nInd-1),waitH);
+            else
+            end
+            kInd = (iInd+1):nInd;
+            tmpCortex = false( 1, nVert ); % initialize 'blank' cortical surface
+            tmpCortex( curIdx( iInd ) ) = true;
+
+            distVect = zeros( 1, nInd-iInd );
+
+            j = 1; % counter controlling the dilation iterations
+            while any( distVect == 0 )
+                k = find(tmpCortex);
+                for iSeed = 1:numel(k)
+                    % dilate all the neighbors of the seed vertex
+                    tmpCortex(vConn{k(iSeed)}) = true;
+                end
+                % don't dilate outside ROI
+                tmpCortex( setdiff( find(tmpCortex) , curIdx ) ) = false;
+
+                % if k indices are in dilated mask, subtrack from distance
+                % indices closer to the i start index will have lower values
+                distVect = distVect - tmpCortex( curIdx(kInd) );
+                j = j + 1;
+                if j == ( opt.maxDist + 1)
+                    % assigning (maxDist - 1) to remaining values means that anything
+                    % beyond the max will be given twice the values max*2 (see below)
+                    distVect( distVect == 0 ) = ( opt.maxDist - 1);
+                    break;
+                end
+            end
+            %if ( -min(distVect) + 1 ) ~= j
+            %        error('unepected');
+            %else
+            %end
+            [distMat(iInd,kInd),distMat(kInd,iInd)] = deal( distVect + j );
+            % old way, benoit
+            % if any(distVect ~= opt.maxDist)
+            %   [distMat(iInd,kInd),distMat(kInd,iInd)] = deal( distVect - min(distVect) + 1 );
+            % else
+            %    [distMat(iInd,kInd),distMat(kInd,iInd)] = deal( distVect ) ;
+            % end
+        end
+        % make diagonal zero
+        distMat(eye(nInd)==1) = 0;
+        % check values
+        allowedValues = [0:opt.maxDist,opt.maxDist*2];
+        curValues = unique(distMat);
+        testIdx = ~ismember(curValues,allowedValues);
+        if any(testIdx)
+            msg = sprintf('\nIllegal value %0.0f in matrix, something''s wrong!\n',curValues(testIdx));
+            error(msg);
+        else
+        end
+    else
+        distMat =  squareform( pdist( subCortex.data.vertices(:,curIdx)' , opt.measure ) );
+    end        
+    if hemiIdx == 1
+        dist = distMat;
+        idx = roiIdx{1};
+    elseif hemiIdx == 2
+        dist = distMat;
+        idx = roiIdx{2};
+    else
+        % struct-style output
+        dist.left = distMat(roiIdx{1},roiIdx{1});
+        dist.right = distMat(roiIdx{2},roiIdx{2});
+        dist.both = distMat;
+        idx.left = roiIdx{1};
+        idx.right = roiIdx{2};
+        idx.both = cat(2,roiIdx{1},roiIdx{2});
+    end
+    if exist('waitH','var')
+        close(waitH);
+    else
+    end
+end
+    
+
+
+
+
+%     %[C, VertConn] = tess_vertices_connectivity(fsL);
+%     
+%     %[D,S,Q] = perform_fast_marching_mesh(fsL.vertices(fIdx{1},:), fsL.faces(fIdx{1},:), 1)
+%     
+%     % compute distances
+%     for h = 1:length(hemis)
+%         % subtract one to get afni zero indexing
+%         col2 = fIdx{h}-1;
+%         afniFile = fullfile(fsDir,'SUMA','lh.pial.asc');
+%         for z = 1:length(fIdx{h})
+%             col1 = repmat(fIdx{h}(z)-1,1,length(fIdx{h}));
+%             tic;
+%             for q = 1:length(col1)
+%                 distMat(q,z,h) =  getDist(col1(q),col2(q),afniFile,opt);
+%             end
+%             toc;
+%         end
+%        
+%     end
+% function distMat = getDist(col1,col2,afniFile,opt)
+%     nameStr = randString(10);
+%     nodeFile = sprintf('/Users/kohler/Desktop/tmp_%s_nodes.1D',nameStr);
+%     outFile = sprintf('/Users/kohler/Desktop/tmp_%s_out.1D',nameStr);
+%     fid=fopen(nodeFile,'w');
+%     fprintf(fid, '%.0f %.0f \n', [col1(:) col2(:)]');
+%     fclose(fid);
+%     clear col*;
+%     if opt.Euclidian
+%         doDist = sprintf('SurfDist -i %s -input %s -Euclidian > %s',afniFile,nodeFile,outFile);
+%     else
+%         doDist = sprintf('SurfDist -i %s -input %s > %s',afniFile,nodeFile,outFile);
+%     end
+%     system(doDist);
+%     [~,~,distMat]=textread(outFile,'%.0f %.0f %f','commentstyle','shell');
+%     system(sprintf('rm %s',outFile));
+%     system(sprintf('rm %s',nodeFile));
+% end
+% 
+% function str = randString(stLength)
+%      symbols = ['a':'z' 'A':'Z' '0':'9'];
+%      nums = randi(numel(symbols),[1 stLength]);
+%      str = symbols(nums);
+%  end
--- +mrC/RoiCorrelation.m
+++ +mrC/RoiCorrelation.m
@@ -1,136 +1,115 @@
-function RoiCorrelation(subId,type)
-% Description:	Generate roi correlation matrices used for doing 
-%               source localization with a functional area constrained
-%               estimator (FACE).
-%               See: Cottereau BR, Ales JM, Norcia AM (2012) Human brain mapping 33:2694-2713.
-%               This is a slightly modified version of a function by B.
-%               Cottereau.
-% Syntax:  mrC.RoiCorrelation(subId, type)
-% In:
-%   subId    - a string specifying the subject ID, without '_fs4' suffix (if given, it will be
-%                 removed
-%   type     - a string specifying the type (or set) of ROIs used for correlation matrix
-%              (['func']/'wangatlas')
+function RoiCorrelation(subId,type,overwrite)
+    % Description:	Generate roi correlation matrices used for doing 
+    %               source localization with a functional area constrained
+    %               estimator (FACE).
+    %               See: Cottereau BR, Ales JM, Norcia AM (2012) Human brain mapping 33:2694-2713.
+    %               This is a slightly modified version of a function by B. Cottereau.
+    % 
+    % Syntax:  mrC.RoiCorrelation(subId, type)
+    % In:
+    %   subId    - a string specifying the subject ID, without '_fs4' suffix (if given, it will be
+    %                 removed
+    %   type     - a string specifying the type (or set) of ROIs used for correlation matrix
+    %              (['func']/'wangatlas')
+    %   overwrite     - a logical indicating whether to overwrite the ROI
+    %                 correlation file (true/[false])              
 
-if nargin < 2
-    type = 'func';
-else
-end
+    if nargin < 2
+        type = 'func';
+    else
+    end
+    if nargin < 3
+        overwrite = true;
+    else
+    end
 
-mrC.SetPrefs; % make sure preferences are set
-if strfind(subId,'_fs4');
-    subId = subId(1:end-4); % get rid of FS suffix, if given by user
-else
-end
+    mrC.SystemSetup; % make sure system is setup
 
-anatDir = fullfile(getpref('mrCurrent','AnatomyFolder'),subId);
-meshDir = fullfile( anatDir, 'Standard', 'meshes' );
-if ~isdir( meshDir )
-	error( 'Subject %s has no /Standard/meshes directory.', subId )
-end
-subCortex = load( fullfile( meshDir, 'defaultCortex.mat' ) );
+    if strfind(subId,'_fs4');
+        subId = subId(1:end-4); % get rid of FS suffix, if given by user
+    else
+    end
 
-[C, VertConn] = ...
-    tess_vertices_connectivity( struct( 'faces',subCortex.msh.data.triangles' + 1, 'vertices',subCortex.msh.data.vertices' ) );
-nVert = numel(VertConn);
+    anatDir = fullfile(getpref('mrCurrent','AnatomyFolder'),subId);
+    meshDir = fullfile( anatDir, 'Standard', 'meshes' );
+    if ~isdir( meshDir )
+        error( 'Subject %s has no /Standard/meshes directory.', subId )
+    end
 
-if strcmp(type, 'func')
-    ROIlist = {'V1d','V1v','V2v','V2d','V3v','V3d','V4','V3ab','LOC','MT','IPS0','VO1'};
-    ROIcorrFile = fullfile( meshDir, 'ROIs_correlation.mat' );
-elseif strfind(type,'wang')
-    type = 'wangatlas';
-    ROIlist = {'V1v' 'V1d' 'V2v' 'V2d' 'V3v' 'V3d' 'hV4' 'VO1' 'VO2' 'PHC1' 'PHC2' ...
-    'TO2' 'TO1' 'LO2' 'LO1' 'V3B' 'V3A' 'IPS0' 'IPS1' 'IPS2' 'IPS3' 'IPS4' ...
-    'IPS5' 'SPL1' 'FEF'};
-    ROIcorrFile = fullfile( meshDir, 'WANG_correlation.mat' );
-else
-    error(['Unknown ROI type: ',type])
-end
+    %% GET FILE AND ROI NAMES
 
-if exist( ROIcorrFile, 'file' )
-	warning([ROIcorrFile,' exists.  Not overwriting.'])
-	return
-end
+    if strcmp(type, 'func')
+        ROIlist = {'V1d','V1v','V2v','V2d','V3v','V3d','V4','V3ab','LOC','MT','IPS0','VO1'};
+        ROIcorrFile = fullfile( meshDir, 'ROIs_correlation.mat' );
+    elseif strfind(type,'wang')
+        type = 'wangatlas';
+        ROIlist = {'V1v' 'V1d' 'V2v' 'V2d' 'V3v' 'V3d' 'hV4' 'VO1' 'VO2' 'PHC1' 'PHC2' ...
+        'TO2' 'TO1' 'LO2' 'LO1' 'V3B' 'V3A' 'IPS0' 'IPS1' 'IPS2' 'IPS3' 'IPS4' ...
+        'IPS5' 'SPL1' 'FEF'};
+        ROIcorrFile = fullfile( meshDir, 'WANG_correlation.mat' );
+    else
+        error(['Unknown ROI type: ',type])
+    end
 
+    if exist( ROIcorrFile, 'file' )
+        if ~overwrite
+            msg = sprintf('\n%s exists. Not overwriting.\n',ROIcorrFile);
+            warning(msg);
+            return
+        else
+            msg = sprintf('\n%s exists, renaming with ''_old'' prefix.\n',ROIcorrFile);
+            disp(msg);
+            movefile(ROIcorrFile,sprintf('%s_old.mat',ROIcorrFile(1:end-4)));
+        end
+    end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Definition of the correlation within each of the ROIs
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Definition of the correlation within each of the ROIs
 
-nROIs = numel(ROIlist) * 2;
-ROIs = struct( 'ndx',{cell(1,nROIs)}, 'corr',{cell(1,nROIs)}, 'name',{reshape([strcat(type,'_',ROIlist,'-L');strcat(type,'_',ROIlist,'-R')],1,nROIs)} );
-max_value = 30;
-cntROI = 0;
-for iROI = 1:nROIs
-    if exist(fullfile( meshDir, 'ROIs', [ROIs.name{iROI},'.mat'] ),'file')
-        cntROI = cntROI+1; % increment counter;
-		
-        load( fullfile( meshDir, 'ROIs', ROIs.name{iROI} ) ); % load ROI
-        ROIs.ndx{cntROI} = ROI.meshIndices;
-        nInd = numel( ROI.meshIndices );
+    nROIs = numel(ROIlist) * 2;
+    ROIs = struct( 'ndx',{cell(1,nROIs)}, 'corr',{cell(1,nROIs)}, 'name',{reshape([strcat(type,'_',ROIlist,'-L');strcat(type,'_',ROIlist,'-R')],1,nROIs)} );
+    cntROI = 0;
+    for iROI = 1:nROIs
+        % loop over complete set of ROIs
+        if exist(fullfile( meshDir, 'ROIs', [ROIs.name{iROI},'.mat'] ),'file')
+            cntROI = cntROI+1; % increment counter;
 
-        dist = 0.5 * eye( nInd );      % Initialization of the distance matrix
-
-        for iInd = 1:(nInd-1)
-
-            kInd = (iInd+1):nInd;
-            tmp = false( 1, nVert );
-            tmp( ROI.meshIndices( iInd ) ) = true;
-
-            distance = zeros( 1, nInd-iInd );
-
-            i = 1;	% #dilation iterations
-            j = 1;
-            while any( distance == 0 )
-
-    % 			tmp = dilatation( tmp , VertConn , i );
-                for iDil = 1:i
-                    k = find(tmp);
-                    for iSeed = 1:numel(k)
-                        tmp(VertConn{k(iSeed)}) = true;
-                    end
-                end
-                % don't dilate outside ROI
-                tmp( setdiff( find(tmp) , ROI.meshIndices ) ) = false;
-
-                distance = distance - tmp( ROI.meshIndices(kInd) );
-                j = j + 1;
-                if j == 30
-                    distance( distance == 0 ) = max_value;
-                    break;
-                end
-            end
-            if any(distance ~= max_value)
-                [dist(iInd,kInd),dist(kInd,iInd)] = deal( distance - min(distance) + 1 );
+            roiPath = fullfile( meshDir, 'ROIs', ROIs.name{iROI} );
+            if strcmp(split_string(ROIs.name{iROI},'-',2),'L')
+                curHemi = 'left';
             else
-                [dist(iInd,kInd),dist(kInd,iInd)] = deal( distance );
+                curHemi = 'right';
             end
-        end
+            [ dist, ROIs.ndx{cntROI} ] = mrC.MeshDist(subId,'ROI',roiPath,'hemi',curHemi);
+            nInd = numel( ROIs.ndx{cntROI} );
+            % mrC.MeshDist outputs zeroes on the diagonal, replace with 0.5
+            dist( eye(nInd)==1 ) = 0.5;
+            ROIs.corr{cntROI} = 0.5 ./ dist + eye(nInd);
+            for iInd = 1 : nInd
+                ROIs.corr{cntROI}( iInd , ROIs.corr{cntROI}(iInd,:) < 0.2 ) = 0;
+            end
 
-        ROIs.corr{cntROI} = 0.5 ./ dist + eye(nInd);
-        for iInd = 1 : nInd
-            ROIs.corr{cntROI}( iInd , ROIs.corr{cntROI}(iInd,:) < 0.2 ) = 0;
+            [V,D] = eig( ROIs.corr{cntROI} );
+            D_tmp = diag( D );
+            if any( D_tmp <= 0 )
+                D_tmp( D_tmp < 0 ) = 0.0001;
+                ROIs.corr{cntROI} = ( V * diag( D_tmp ) * inv( V ) );
+            end
+            included(cntROI)={ROIs.name{iROI}};
+        else
         end
-
-        [V,D] = eig( ROIs.corr{cntROI} );
-        D_tmp = diag( D );
-        if any( D_tmp <= 0 )
-            D_tmp( D_tmp < 0 ) = 0.0001;
-            ROIs.corr{cntROI} = ( V * diag( D_tmp ) * inv( V ) );
-        end
-        included(cntROI)={ROIs.name{iROI}};
-    else
     end
-end
 
-if exist('included','var')
-    not_included = find(~ismember(ROIs.name,included));
-    if ~isempty(not_included)
-        disp(cell2mat(['NOT INCLUDED:',cellfun(@(x) [' ',x],ROIs.name(not_included),'uni',false)]))
+    if exist('included','var')
+        not_included = find(~ismember(ROIs.name,included));
+        if ~isempty(not_included)
+            disp(cell2mat(['NOT INCLUDED:',cellfun(@(x) [' ',x],ROIs.name(not_included),'uni',false)]))
+        else
+        end
+        save( ROIcorrFile, 'ROIs' )
     else
+        warning('No ROIs! Not creating matrix')
     end
-    save( ROIcorrFile, 'ROIs' )
-else
-    warning('No ROIs! Not creating matrix')
 end
 
 
