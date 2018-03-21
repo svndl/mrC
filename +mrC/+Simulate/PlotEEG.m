0be07c5ff7c423b4b733f341b1b1b57630135a76
EBarzegaran <e.barzegaran@gmail.com>
2018-03-20 17:27:33 -0700

    Updating simulate plot function

--- +mrC/+Simulate/PlotEEG.m
+++ +mrC/+Simulate/PlotEEG.m
@@ -1,4 +1,4 @@
-function PlotEEG(ASDEEG,Freq,FF,Probs,savepath,masterList,signalFF)
+function PlotEEG(ASDEEG,Freq,savepath,subID, masterList,signalFF)
 % This function provides a dual plot of electrode spectrum and topographic
 % map of amplitude at a specific frequency (similar to powerDiva)
 
@@ -13,35 +13,52 @@ function PlotEEG(ASDEEG,Freq,FF,Probs,savepath,masterList,signalFF)
     % ASDEEG: nf x nelec matrix, amplitude spectrum of EEG, nf is number of
             % frequency bins and nelec is number of electrodes
     % Freq: nf x 1 vector, indicating the frequency bins
-    % FF: fundamental frequency indexes
-    % Probs: the plotting params
     % savepath: the string input indicating the folder to save the plots
     % master list: a cell array containing the names of ROIs
     
 % Written by ELham Barzegaran,
 % Last modification: 3.7.2018
 
-%%
-% set parameters
+%% set parameters
+
 FS=14;%font size
-Fmax = numel(Freq);
+Fmax = numel(Freq); % maximum frequency
 EOI = 83; % electrode used in the plot
-FOI = FF(1); % frequency bin used in the plot
 load('Electrodeposition.mat'); tEpos =tEpos.xy;% electrode poritions used for plots
+
+if isempty(signalFF), 
+    FFI=2; 
+else 
+    FFI = signalFF;
+end
+
+[~,~,FFI] = intersect(FFI,round(Freq*1000)/1000);% find the index of fundamental frequencies in frequency bins and keep them for plotting purpose
+FOI = FFI(1);
+
+%% Plot prepration
+Probs{1} = {'facecolor','none','edgecolor','none','markersize',10,'marker','o','markerfacecolor','g' ,'MarkerEdgeColor','k','LineWidth',.5};% plotting parameters
+conMap = jmaColors('hotcortex');
 
 h=figure;
 set(h,'units','centimeters')
-set(h, 'position',[1 1 25 12]);
+set(h, 'Position',[10 10 35 16]);
+set(h,'PaperPositionMode','manual')
 
-subplot(2,2,4),axis off;% Show simulated signal information
+
+subplot(2,2,2),axis off;% Show simulated signal information
 nrs = max(numel(masterList),3);
+text(.1,1,['Subject ' subID],'fontsize',FS+1,'fontweight','bold');
 for i= 1:numel(masterList)
     if ~isempty(signalFF)
-        text (.1,.9-((1/nrs)*(i)),['Source' num2str(i) ': f' num2str(i) ' = ' num2str(signalFF(i)) ' Hz,  ' strrep(masterList{i},'_','-')],'fontsize',FS-2);
+        text (.1,1-((.15)*(i)),['Source' num2str(i) ': f' num2str(i) ' = ' num2str(signalFF(i)) ' Hz,  ' strrep(masterList{i},'_','-')],'fontsize',FS-2);
     else
-        text (.1,.9-((1/nrs)*(i)),['Source' num2str(i) ': ' strrep(masterList{i},'_','-')],'fontsize',FS-2);
+        text (.1,1-((.15)*(i)),['Source' num2str(i) ': ' strrep(masterList{i},'_','-')],'fontsize',FS-2);
     end
 end
+set(gca,'tag','info');
+
+%% Loop over plots
+
 N = 1;
 while(1)
     if N == 1, colorbarLimits = [-0 max(ASDEEG(FOI,:))];
@@ -49,17 +66,23 @@ while(1)
     end
     
     if exist('sp1','var'),delete(sp1);end % topography map plot
-    sp1 = subplot(1,2,1);mrC.plotOnEgi(ASDEEG(FOI,:)',colorbarLimits,false,EOI,false,Probs); 
-    title(['ASD topographic map (Frequency = ' num2str(Freq(FOI)) 'Hz)'],'fontsize',FS);set(gca,'tag',num2str(1));
+    sp1 = subplot(1,2,1);
+    mrC.plotOnEgi(ASDEEG(FOI,:)',colorbarLimits,false,EOI,false,Probs); 
+    title(['Frequency = ' num2str(Freq(FOI)) 'Hz'],'fontsize',FS);
+    set(sp1,'tag',num2str(1));
     colorbar;
     
     if exist('sp2','var'),delete(sp2);end % spectrum plot
-    sp2 = subplot(2,2,2); bar(Freq(1:Fmax), ASDEEG(1:Fmax,EOI),.15); 
-    xlim([Freq(1) Freq(Fmax)]);xlabel('Frequency(Hz)','fontsize',FS);
-    ylim([0 max(ASDEEG(1:Fmax,EOI))*1]);ylabel('ASD','fontsize',FS);set(gca,'tag',num2str(2));
-    hold on; bar(Freq(FOI), ASDEEG(FOI,EOI),.3,'g'); 
-    %set(sp2, 'Position',get(sp2, 'Position')+[0 .4 0 -.4] );
-    title(['ASD at Electrode ' num2str(num2str(EOI))],'fontsize',FS);
+    sp2 = subplot(2,2,4); 
+    bar(Freq(1:Fmax), ASDEEG(1:Fmax,EOI),.15); 
+    xlim([Freq(1) Freq(Fmax)]);
+    xlabel('Frequency(Hz)','fontsize',FS-2);
+    ylim([0 max(ASDEEG(1:Fmax,EOI))*1.1]);
+    ylabel('ASD','fontsize',FS-2);
+    set(sp2,'tag',num2str(2));
+    hold on; 
+    bar(Freq(FOI), ASDEEG(FOI,EOI),.4,'FaceColor','g','EdgeColor','g'); 
+    title(['Electrode ' num2str(num2str(EOI))],'fontsize',FS);
     
     %% Reads keyboard or mouse click
     w = waitforbuttonpress;
@@ -70,7 +93,8 @@ while(1)
           close;
           return
       elseif key==13 % (Enter key) -> save the current figure
-          print(fullfile(savepath,['SimEEG_Electrode' num2str(EOI) '_Freq' num2str(Freq(FOI)) 'Hz.tif']),'-dtiff','-r300');% Later I can update this to contain the simulation parameters
+          set(h, 'PaperPosition',[1 1 12 5]);
+          print(fullfile(savepath,['SimEEG_Subject' subID 'Electrode' num2str(EOI) '_Freq' num2str(Freq(FOI)) 'Hz.tif']),'-dtiff','-r300');% Later I can update this to contain the simulation parameters
       elseif strcmp(key,'n')||strcmp(key,'N') % if n or N is pressed -> change head plot normalization
           if N==1, N=0; else N=1;end
       end
@@ -80,17 +104,19 @@ while(1)
       SPI = get(gca,'tag');
       x = mousept(1,1);
       y = mousept(1,2);
-    end
-    
-    % update location
-    switch SPI
+      % update location
+      switch SPI
         case '1'
             Epos2= repmat([x y],[128 1]);
             dis = sqrt(sum((tEpos-Epos2).^2,2));
             [~,EOI] = min(dis);
-           
+
         case '2'
             [~,FOI] = min(abs(repmat(x,[1 size(ASDEEG,1)])-Freq));
+      end
     end
+    
+    
+
 end
 end
