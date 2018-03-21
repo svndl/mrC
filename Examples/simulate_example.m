501922a77de9f50a7edc6723b409f92ffb88957b
EBarzegaran <e.barzegaran@gmail.com>
2018-03-20 17:33:23 -0700

    Update example

--- Examples/simulate_example.m
+++ Examples/simulate_example.m
@@ -11,21 +11,31 @@
 clear;clc
 
 %% Add latest mrC
-codeFolder = 'C:\Users\Elhamkhanom\Documents\Codes\Git\tmpmrC';
-addpath(genpath(fullfile(codeFolder,'mrC')));
+mrCFolder = fileparts(pwd);%'/Users/kohler/code/gits';
+addpath(genpath(mrCFolder));
 
 %% SSVEP signal can be simulated using ModelSourceSignal with defined parameters, otherwise Roisignal function will generate a default two source SSVEP signal 
 % a sample SSVEP signal...
-[outSignal, FundFreq, SF]= mrC.Simulate.ModelSeedSignal('signalType','SSVEP','signalFreq',[2 3.5 5],'signalHarmonic',{[2,0,1],[0,1,0,2],[2, 2]},'signalPhase',{[.1,0,.2],[0,.3,0,.4],[.2,.5]});
+
+%[outSignal, FundFreq, SF]= mrC.Simulate.ModelSeedSignal('signalType','SSVEP','signalFreq',[2 3.5 5],'signalHarmonic',{[2,0,1],[0,1,0,2],[2, 2]},'signalPhase',{[.1,0,.2],[0,.3,0,.4],[.2,.5]});
+
 %[sensorData,masterList,subIDs] = mrC.Simulate.RoiSignal(ProjectPath,'anatomyPath',AnatomyPath,'signalArray',outSignal);
 
 %% One subject mrC project and anatomy paths
-DataFolder = 'C:\Users\Elhamkhanom\Documents\My works\StanfordWorks\simulation\';
-ProjectPath{1} = fullfile(DataFolder,'Example','nl-0014_ssn2'); % example folder...
-AnatomyPath = fullfile(DataFolder,'Example','anatomy');
+% Example subject
+% DataFolder = 'C:\Users\Elhamkhanom\Documents\My works\StanfordWorks\simulation\';
+% ProjectPath{1} = fullfile(DataFolder,'Example','nl-0014_ssn2'); % example folder...
+% AnatomyPath = fullfile(DataFolder,'Example','anatomy');
+
+ProjectPath = '/Volumes/svndl/4D2/kohler/SYM_16GR/SOURCE';
+ProjectPath = subfolders(ProjectPath,1); ProjectPath = ProjectPath(1:2:end); % All subjects in this project, just for the first session
+
+AnatomyPath = '/Volumes/svndl/anatomy';
+
 %% simulation functions
 noise.mu=3;
 %noise.distanceType = 'Geodesic';
-[sensorData,masterList,subIDs] = mrC.Simulate.RoiSignal(ProjectPath,'anatomyPath',AnatomyPath,'noiseParams',noise);
 
+[sensorData,masterList,subIDs] = mrC.Simulate.RoiSignal(ProjectPath,'anatomyPath',AnatomyPath,'roiType','func','noiseParams',noise);
 
+%%
