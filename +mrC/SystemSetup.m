d20053be28420ab534967ddf633757eaf0487450
Peter J. Kohler, PhD <pjkohl3r@gmail.com>
2018-03-30 13:33:31 -0700

    added nearpoints to external, removed from mrC.SystemSetup (+headless flexibility)

--- +mrC/SystemSetup.m
+++ +mrC/SystemSetup.m
@@ -1,45 +1,38 @@
-function [fsDir,anatDir] = SystemSetup
+function SystemSetup(analysis)
     % mrC.SystemSetup: check if folder preferences are set, if not, set them
     
-    %% CHECK IF NECESSARY FOLDERS ARE SET AS ENVIRONMENT VARIABLES AND EXIST
-    
-    % mrCurrent AnatomyFolder
-    
-    if ~ispref('mrCurrent','AnatomyFolder')
-        anatDir = '/Volumes/svndl/anatomy';
-        if ~exist(anatDir,'dir')
-            anatDir = uigetdir(anatDir,'Anatomy directory?');
-        else
-        end
-        setpref('mrCurrent','AnatomyFolder',anatDir);
-        fprintf('Anatomy folder set: %s\n',anatDir);
+    if nargin < 1
+        analysis = 'source';
     else
-         anatDir = getpref('mrCurrent','AnatomyFolder');
     end
-    
-    % freesurfer subject directory
-    if ~ispref('freesurfer','SUBJECTS_DIR')
+
+    if ismember(lower(analysis),{'source','sourcelocalization'})
         fsDir = '/Volumes/svndl/anatomy/FREESURFER_SUBS';
-        if ~exist(fsDir,'dir')
-            fsDir = uigetdir(fsDir,'Freesurfer directory?');
-        else
-        end
-        setpref('freesurfer','SUBJECTS_DIR',fsDir)
-        fprintf('Freesurfer folder set: %s\n',fsDir);
+        anatDir = '/Volumes/svndl/anatomy';
+    elseif ismember(lower(analysis),{'headless'})
+        fsDir = '/Volumes/svndl/anatomy/FREESURFER_SUBS';
+        anatDir = '/Volumes/svndl/anatomy/HEADLESS';
     else
-        fsDir = getpref('freesurfer','SUBJECTS_DIR');
+        msg = sprintf('\n unknown analysis %s provided to mrC.SystemSetup, must be ''source'' or ''headless''\n');
+        error(msg)
     end
-    
-    
-    %% CHECK IF NECESSARY DEPENDENCIES ARE INSTALLED
-    % check if mrVista is installed and on path
-    if exist('nearpoints') ~= 3,
-        error(sprintf(strcat(2,...
-            ['\n nearpoints mex-file not found. \n', ...
-             'please install from http://github.com/vistalab/vistasoft. \n', ...
-             'and make sure to add to vistasoft folder to path.' ])));
+    % CHECK IF FOLDERS THAT WILL BE SET AS ENVIRONMENT VARIABLES EXIST
+    % mrCurrent AnatomyFolder
+    if ~exist(anatDir,'dir')
+        anatDir = uigetdir(anatDir,'Anatomy directory?');
     else
     end
+    % freesurfer subject directory
+    fsDir = '/Volumes/svndl/anatomy/FREESURFER_SUBS';
+    if ~exist(fsDir,'dir')
+        fsDir = uigetdir(fsDir,'Freesurfer directory?');
+    else
+    end
     
+    % set environment variables
+    setpref('mrCurrent','AnatomyFolder',anatDir);
+    fprintf('Anatomy folder set: %s\n',anatDir);
+    setpref('freesurfer','SUBJECTS_DIR',fsDir)
+    fprintf('Freesurfer folder set: %s\n',fsDir);
 end
 
