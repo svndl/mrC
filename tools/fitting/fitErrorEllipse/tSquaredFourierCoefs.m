58aa9fe2511750072ba15a92e5042a5a5c482296
Peter J. Kohler, PhD <pjkohl3r@gmail.com>
2018-05-03 14:29:08 -0700

    multivariate effect size

--- tools/fitting/fitErrorEllipse/tSquaredFourierCoefs.m
+++ tools/fitting/fitErrorEllipse/tSquaredFourierCoefs.m
@@ -46,8 +46,10 @@ function [results] = tSquaredFourierCoefs(xyData,varargin)
     if dims(2) ~= 2
         error('input data must be a matrix of 2D row samples');
     end
+    twoSamples = true;
     if length(dims) < 3 % if no third dimension
         xyData(:,:,2) = zeros(size(xyData));
+        twoSamples = false;
     elseif dims(3) > 2
          error('length of third dimension of input data may not exceed two')
     else
@@ -56,20 +58,21 @@ function [results] = tSquaredFourierCoefs(xyData,varargin)
     if length(opt.testMu) ~= 2
         error('testMu should be a 2D vector');
     end
-
-    xyData = xyData(:,:,1) - xyData(:,:,2);
     
+    % between condition differences
+    xy_diff = xyData(:,:,1) - xyData(:,:,2);
     % remove NaNs
-    xyData = xyData(~any(isnan(xyData),2),:);
+    notNaNs = ~any(isnan(xy_diff),2);
+    xy_diff = xy_diff(notNaNs,:);
+    xyData = xyData(notNaNs,:,:);
     
-    N = size(xyData,1);
-    
+    N = size(xy_diff,1);
     try
-        [sampMu,sampCovMat] = eigFourierCoefs(xyData);
+        [sampMu,sampCovMat] = eigFourierCoefs(xy_diff);
     catch
         fprintf('The covariance matrix of xyData could not be calculated, probably your data do not contain >1 sample.');
     end
-
+        
     if opt.pdStyle
         sampCovMat = eye(2);
     else
@@ -92,15 +95,44 @@ function [results] = tSquaredFourierCoefs(xyData,varargin)
 
         tSqrdF = (N-p)/(p*(N-1)) * tSqrd; % F approximation 
         pVal = 1 - fcdf(tSqrdF, df1, df2);  % compute p-value
-
-        results.tSqrd = tSqrd;
-        results.pVal = pVal;
-        results.H = tSqrd >= results.tSqrdCritical;
-        results.df1 = df1;
-        results.df2 = df2;
-        results.testAmp = abs(complex(sampMu(1),sampMu(2)));
     catch
         fprintf('inverse of the sample covariance matrix could not be computed.')
+        return
     end
+    if twoSamples
+        % standard deviations for real and imaginary values
+        xy_std = std(xyData,0,1);
+        % univariate effect size for real and imaginary
+        % standardize by average standard deviation over condtions
+        % (d_av, equation 10, Läkens, Frontiers in Psych, 2013)
+        xy_d = sampMu./sum(xy_std,3);
+        
+        % compute covariance between real and imaginary values, 
+        % and standardize by standard deviation
+        % then average over conditions
+        % (Del Giudice, personal communication, 2018)
+        corMat = (cov([xyData(:,1,1),xyData(:,2,1)])/(xy_std(:,1,1)*xy_std(:,2,1)) + ...
+                  cov([xyData(:,1,2),xyData(:,2,2)])/(xy_std(:,1,2)*xy_std(:,2,2)))/2;
+
+        corMat(eye(2)==1) = 1;
+        % Mahalanobis' D (equation 1, Del Giudice, Evo. Psych., 2009)
+        mahaD = sqrt( xy_d * inv(corMat) * xy_d' );
+        % overlapping coefficient (equation 2, Del Giudice, Evo. Psych., 2009)
+        OVL = 2*normcdf(-mahaD/2);
+        % Cohen?s index of nonoverlap (equation 3, Del Giudice, Evo. Psych., 2009)
+        nonOverlap = 1-OVL/(2-OVL); 
+    else
+        % note that overlap indices do not apply here
+        mahaD = sqrt(tSqrd/N);
+    end
+    results.tSqrd = tSqrd;
+    results.pVal = pVal;
+    results.H = tSqrd >= results.tSqrdCritical;
+    results.df1 = df1;
+    results.df2 = df2;
+    results.testAmp = abs(complex(sampMu(1),sampMu(2)));
+    results.mahalanobisD = mahaD;
+    if twoSamples
+        results.cohenNonOverlap = nonOverlap;
 end
 
