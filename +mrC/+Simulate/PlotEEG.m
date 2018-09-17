d1346b7466eb2bc85e2d0ef05666affed0764ec3
Sebastian Bosse <sebastian.bosse@hhi.fraunhofer.de>
2018-09-17 21:36:00 +0200

    added keyboard control for component visualization

--- +mrC/+Simulate/PlotEEG.m
+++ +mrC/+Simulate/PlotEEG.m
@@ -1,4 +1,4 @@
-function h = PlotEEG(ASDEEG,Freq,savepath,subID, masterList,signalFF,SignalType,jcolors,Mode,EOI,FOI,A)
+function h = PlotEEG(ASDEEG,Freq,savepath,subID, masterList,signalFF,SignalType,jcolors,Mode,EOI,FOI,A,W)
 % This function provides a dual plot of electrode amplitude/phase spectrum and topographic
 % map of amplitude/phase at a specific frequency (similar to powerDiva)
 
@@ -24,9 +24,11 @@ function h = PlotEEG(ASDEEG,Freq,savepath,subID, masterList,signalFF,SignalType,
     % FOI: the frequency to plot the (initial) spectrum topo map
     
     
-% Written by ELham Barzegaran,3.7.2018
-% Latest modification: 6.6.2018
+% Written by Elham Barzegaran,3.7.2018
+% modified by EB: 6.6.2018
 % modified by sebastian bosse 8.8.2018
+% modified by EB: 8.21.2018
+% modified by sb: 9.17.2018
 
 %% set parameters
 if ~exist('SignalType','var')|| isempty(SignalType) % plot phase or amp
@@ -69,6 +71,8 @@ else
     space = 'comp';
     EOI = 1 ; % start with the first component
     A = abs(A) ; % visualization in amplitude domain
+    %Mode = '';
+    weight = 1;
 end
 
 %% Plot prepration
@@ -104,12 +108,33 @@ for i= 1:numel(masterList)
 end
 set(gca,'tag','info');
 
+
+%% draw buttons for component selection
+if strcmpi(space,'comp')
+    ax1 = axes('Units','pixel','Position',[30 400 50 10],'Visible','off');
+    axes(ax1);
+    text(0, 0,'RC number','fontweight','bold')
+    popup = uicontrol('Style', 'popup',...
+                      'String', '1|2|3|4|5|6|7|8|9',...
+                      'Position', [20 340 100 50],...
+                      'Callback',@set_component);
+    % plot which weight
+    ax2 = axes('Units','pixel','Position',[30 360 50 10],'Visible','off');
+    axes(ax2);
+    text(0, 0,'Weight','fontweight','bold')
+    popup = uicontrol('Style', 'popup',...
+                  'String', 'A|W',...
+                  'Position', [20 300 100 50],...
+                  'Callback',@set_weight);
+end
+
+
 %% Loop over plots
 
 N = 1;
 I = 1;    
 while(I)
-    if strcmp(SignalType,'Amplitude')
+    if strcmpi(SignalType,'Amplitude')
         if strcmpi(space,'comp') 
             colorbarLimits = [-0 max(A(:,EOI)*ASDEEG(FOI,EOI)')];
         else
@@ -117,49 +142,22 @@ while(I)
             else colorbarLimits = [-0 max(ASDEEG(:))];
             end
         end
-    elseif strcmp(SignalType,'Phase')
+    elseif strcmpi(SignalType,'Phase')
         colorbarLimits = [min(ASDEEG(:)) max(ASDEEG(:))];
     end
-    
-    if exist('sp1','var'),delete(sp1);end % topography map plot
-        sp1 = subplot(1,2,1);
-    if strcmpi(Mode,'Interact') && ~strcmpi(space,'comp'),
-        mrC.plotOnEgi(ASDEEG(FOI,:)',colorbarLimits,false,EOI,false,Probs); 
-    else
-        if strcmpi(space,'comp') 
-            mrC.plotOnEgi(A(:,EOI)*ASDEEG(FOI,EOI)',colorbarLimits,false,[],false,Probs); 
-        else
-            mrC.plotOnEgi(ASDEEG(FOI,:)',colorbarLimits,false,[],false,Probs); 
-        end
-    end
-    title(['Frequency = ' num2str(Freq(FOI)) 'Hz'],'fontsize',FS);
-    set(sp1,'tag',num2str(1));
-    colormap(conMap);
-    colorbar;
-    
-    if exist('sp2','var'),delete(sp2);end % spectrum plot
-        sp2 = subplot(2,2,4); 
-    bar(Freq(1:Fmax), ASDEEG(1:Fmax,EOI),.15); 
-    xlim([Freq(1) Freq(Fmax)]);
-    xlabel('Frequency(Hz)','fontsize',FS-2);
-    if strcmp(SignalType,'Amplitude')
-        ylim([0 max(ASDEEG(1:Fmax,EOI))*1.1]);
-        ylabel('ASD','fontsize',FS-2);
-    elseif strcmp(SignalType,'Phase')
-        ylim([min(ASDEEG(1:Fmax,EOI))*1.1 max(ASDEEG(1:Fmax,EOI))*1.1]);
-        ylabel('Phase(degree)','fontsize',FS-2);
-    end
-    set(sp2,'tag',num2str(2));
-    hold on; 
-    bar(Freq(FOI), ASDEEG(FOI,EOI),.4,'FaceColor','g','EdgeColor','g'); 
-    
-    % draw buttons for channel selection
+
+
+    %---------------------------HEAD PLOT----------------------------------
+    plot_topog();
+    %---------------------------SPECTRUM PLOT------------------------------
+    plot_spectrum();
+    %----------------------------------------------------------------------
     if strcmpi(space,'comp')
         title(['Component ' num2str(num2str(EOI))],'fontsize',FS);
     else
         title(['Electrode ' num2str(num2str(EOI))],'fontsize',FS);
     end
-    %% Reads keyboard or mouse click
+    %% ----------------Reads keyboard or mouse click-----------------------
     if strcmpi(Mode,'Interact')
         w = waitforbuttonpress;
         switch w 
@@ -225,6 +223,7 @@ while(I)
             y = mousept(1,2);
             % update location
             switch SPI
+
                 case '1'
                     if ~strcmpi(space,'comp')
                         Epos2= repmat([x y],[128 1]);
@@ -233,12 +232,70 @@ while(I)
                     end
                 case '2'
                     [~,FOI] = min(abs(repmat(x,[1 size(ASDEEG,1)])-Freq));
+              end
             end
-        end
     else
         I = 0;
     end
 end
 
-%print(fullfile(savepath,['SimEEG_Subject' subID 'Electrode' num2str(EOI) '_Freq' num2str(Freq(FOI)) 'Hz_' SignalType  '.tif']),'-dtiff','-r300');% Later I can update this to contain the simulation parameters
+    %----------------------------------------------------------------------
+    function set_component(source,event)
+        EOI = get(source,'value');
+        plot_topog();
+        plot_spectrum();
+    end
+
+    function set_weight(source,event)
+        weight = get(source,'value');
+        plot_topog();
+    end
+%--------------------------------------------------------------------------
+    function plot_topog()
+        % topography map plot
+        
+        sp1 = subplot(1,2,1);delete(sp1);
+        sp1 = subplot(1,2,1);
+        if strcmpi(Mode,'Interact') && ~strcmpi(space,'comp'),
+            mrC.plotOnEgi(ASDEEG(FOI,:)',colorbarLimits,false,EOI,false,Probs); 
+        else
+            if strcmpi(space,'comp') 
+                if weight==1
+                    conMap = jmaColors('hotcortex');
+                    mrC.plotOnEgi(A(:,EOI)*ASDEEG(FOI,EOI)',colorbarLimits,false,[],false,Probs); 
+
+                else
+                    mrC.plotOnEgi(W(:,EOI),[-max(abs(W(:,EOI))) max(abs(W(:,EOI)))],false,[],false,Probs);
+                    conMap = jmaColors('coolhotcortex');
+                end
+            else
+                mrC.plotOnEgi(ASDEEG(FOI,:)',colorbarLimits,false,[],false,Probs); 
+            end
+        end
+        title(['Frequency = ' num2str(Freq(FOI)) 'Hz'],'fontsize',FS);
+        set(sp1,'tag',num2str(1));
+        colormap(conMap);
+        colorbar;
+    end
+%--------------------------------------------------------------------------
+    function plot_spectrum() 
+         % spectrum plot
+        SPEC = ASDEEG(1:Fmax,EOI);
+        sp2 = subplot(2,2,4); delete(sp2);
+        sp2 = subplot(2,2,4); 
+        bar(Freq(1:Fmax), SPEC,.15); 
+        xlim([Freq(1) Freq(Fmax)]);
+        xlabel('Frequency(Hz)','fontsize',FS-2);
+        if strcmp(SignalType,'Amplitude')
+            ylim([min(SPEC) max(SPEC)*1.1]);
+            ylabel('ASD','fontsize',FS-2);
+        elseif strcmp(SignalType,'Phase')
+            ylim([min(SPEC)*1.1 max(SPEC)*1.1]);
+            ylabel('Phase(degree)','fontsize',FS-2);
+        end
+        set(sp2,'tag',num2str(2));
+        
+        if strcmpi(Mode ,'Interact'),hold on;  bar(Freq(FOI), SPEC(FOI),.4,'FaceColor','g','EdgeColor','g'); end
+
+    end
 end
