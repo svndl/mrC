a486a8cc0538f0ad419a3e129143065c1fa90515
Sebastian Bosse <sebastian.bosse@hhi.fraunhofer.de>
2018-11-05 15:26:14 +0100

    added svd-based decomposition for increased numerical stability and to obtain less sparse mixing matrix

--- +mrC/+Simulate/GenerateMixingData.m
+++ +mrC/+Simulate/GenerateMixingData.m
@@ -1,4 +1,4 @@
-function noise_mixing_data = GenerateMixingData(spat_dists)
+function noise_mixing_data = GenerateMixingData(spat_dists, decomp_algo)
 % Syntax: noise_mixing_data = GenerateMixingData(spat_dists)
 % Description: This function get the spatial distance between sources and loads the
 % spatial decay model for coherenc and generates the mixing matrix for noise
@@ -8,6 +8,11 @@ function noise_mixing_data = GenerateMixingData(spat_dists)
 % 'perceptual effects' are expected
 
 %% --------------------------------------------------------------------------
+
+if ~exist('decomp_algo','var') || isempty(decomp_algo) 
+    decomp_algo = 'cholesky';
+end
+
 
 % preparing mixing matrices
     load('spatial_decay_models_coherence')% this is located in simulate/private folder, it can be obtained by run the code 'spatial_decay_of_coherence.m'
@@ -19,15 +24,29 @@ function noise_mixing_data = GenerateMixingData(spat_dists)
     noise_mixing_data.band_freqs = band_freqs ;
     noise_mixing_data.mixing_type = 'coh' ;
 
+    % we are assuming isolation between hemispheres: calculate mixing
+    % matrices seperately
+    
     hWait = waitbar(0,'Calculating mixing matrices ... ');
        
     for freq_band_idx = 1:length(band_freqs)
         this_spatial_decay_model = best_model.(band_names{freq_band_idx}) ;
+        mixing_matrix = zeros(size(spat_dists,1),size(spat_dists,2)) ;
+        
+        
         this_coh = this_spatial_decay_model.fun(this_spatial_decay_model.model_params,spat_dists);
         this_coh = min(max(this_coh,0),1) ;
-        mixing_data =  chol(this_coh) ; % this matrix should become sparse, to be saved
-       
-        noise_mixing_data.matrices{freq_band_idx} = sparse(mixing_data .* (mixing_data>0.01)); %make the matrix sparse for saving
+                
+        if strcmpi(decomp_algo,'cholesky')
+            this_mixing_matrix =  chol(this_coh) ;
+        elseif strcmpi(decomp_algo,'eigenvalue')
+            [V,D] =eig(this_coh);
+            this_mixing_matrix = sqrt(D)*V' ;
+        else
+            error('decomposition method not implemented')
+        end
+        
+        noise_mixing_data.matrices{freq_band_idx} = this_mixing_matrix;
         waitbar(freq_band_idx/length(band_freqs));
     end       
     close(hWait);
