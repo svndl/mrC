e1bc834e1b1bcdff2ab7eb148900662428732d2e
sebobosse <sebastian.bosse@gmail.com>
2019-01-24 21:54:18 +0100

    corrections in values of coherence; aligned data structure of coherence model

--- +mrC/+Simulate/private/SpatialDecayofCoherence.m
+++ +mrC/+Simulate/private/SpatialDecayofCoherence.m
@@ -1,7 +1,7 @@
 % estimate and write out functional model and  parameters for
-% distance-dependence of coherence (according to Fig. 4 from 'Multi-scale
+% distance-dependence of coherence (according to MAE1 numbers Fig. 4 from 'Multi-scale
 % analysis of neural activity in humans: Implications for 
-% micro-scale electrocorticography'
+% micro-scale electrocorticography')
 clear all
 
 model_fun.gauss = @(p,x)(p(4)+p(1)*exp(-((x-p(3))/2*p(2)^2)));
@@ -9,25 +9,36 @@ model_fun.expo   =  @(p,x)(p(4)+p(1)*exp(-p(2)*(x-p(3))));
 model_fun.lorentzian   =  @(p,x)(p(4)+p(1)* ((p(2)^2)./(p(2)^2+(x-p(3)).^2)));
 model_fun.power_law =  @(p,x)(p(4)+p(1)* (x+p(3)).^(-p(2)));
 
+
+
 % data taken from Fig. 4
-x = [0,0.38,0.5,0.54,1:0.5:3] % in mm
+x = [0,0.38,0.5,0.54,1:0.5:3] ;% in mm
 y.delta = [7., 6.3, 6.1, 6.05, 5.5,  5.3,  5,    4.9,  4.8]/7; % strange number where read from print in cm
 y.theta = [7., 6.1, 5.8, 5.7,  5.1,  4.8,  4.5,  4.4,  4.3]/7;
 y.beta =  [7., 6.3, 6.1, 6.05, 5.75, 5.5,  5.25, 5.1,  5]/7;
 y.alpha = [7., 5.1, 4.9, 4.8,  4.45, 4.2,  3.9,  3.8,  3.6]/7;
 y.gamma = [7., 3.2, 2.9, 2.75, 2.3,  2.05, 1.9,  1.75, 1.6]/7;
 
-band_freqs{1} = [0,4];
-band_freqs{2} = [4,8];
-band_freqs{3} = [8,12];
-band_freqs{4} = [12,30];
-band_freqs{5} = [30,80];
 
+x =       [0., 0.38, 0.5,   0.54, 1.,   1.5,  2.,   2.5,  3.,   3.5,  4.,   4.5,  5.] ;% in mm
+y.delta = [1., 0.85, 0.825, 0.81, 0.72, 0.67, 0.62, 0.61, 0.61, 0.61, 0.61, 0.61, 0.61] ;
+y.theta = [1., 0.9,  0.87,  0.85, 0.8,  0.73, 0.7,  0.67, 0.67, 0.67, 0.67, 0.67, 0.67] ;
+y.beta =  [1., 0.73, 0.7,   0.68, 0.63, 0.6,  0.55, 0.63, 0.5,  0.48, 0.46, 0.44, 0.4 ];
+y.alpha = [1., 0.9,  0.87,  0.81, 0.82, 0.79, 0.75, 0.725, 0.71, 0.68, 0.66, 0.62, 0.6 ] ;
+y.gamma = [1., 0.45, 0.42,  0.39, 0.33, 0.3 , 0.27, 0.25,  0.22, 0.21,  0.2,  0.2, 0.19];
+
+
+band_freqs.delta = [0,4];
+band_freqs.theta = [4,8];
+band_freqs.alpha = [8,12];
+band_freqs.beta = [12,30];
+band_freqs.gamma = [30,80];
+%%
 fig=figure ;
 freq_band_names = fieldnames(y) ;
 model_names = fieldnames(model_fun) ;
 
-x_temp = [0:0.5:4];
+x_temp = [min(x):0.1:100*max(x)];
 
 colors = {'r','g','b','k','c','m','y'};
 linestyles = {'-','--',':','-.'};
@@ -35,7 +46,7 @@ linestyles = {'-','--',':','-.'};
 handles = [];
 for i = 1:length(freq_band_names)
     this_bands_mse = 100000;
-    
+    this_band_name = freq_band_names{i};
     for this_model_idx = 1:length(model_names)
         subplot(1,length(model_names),this_model_idx );
         this_model_name = model_names{this_model_idx} ;
@@ -43,28 +54,29 @@ for i = 1:length(freq_band_names)
         hold on 
         if strcmp(this_model_name,'power_law')
              if strfind(version,'R2011')
-             [this_p,R,J,CovB,MSE] =nlinfit(x(1:end),y.(freq_band_names{i})(1:end),model_fun.(this_model_name),[1.,1.,1.   ,0.]);
+             [this_p,R,J,CovB,MSE] =nlinfit(x(1:end),y.(this_band_name)(1:end),model_fun.(this_model_name),[1.,1.,1.   ,0.]);
              else
-             [this_p,R,J,CovB,MSE,ErrorModelInfo] =nlinfit(x(1:end),y.(freq_band_names{i})(1:end),model_fun.(this_model_name),[1.,1.,1.   ,0.]);
+             [this_p,R,J,CovB,MSE,ErrorModelInfo] =nlinfit(x(1:end),y.(this_band_name)(1:end),model_fun.(this_model_name),[1.,1.,1.   ,0.]);
              end
         else
             if strfind(version,'R2011')
-            [this_p,R,J,CovB,MSE] =nlinfit(x(1:end),y.(freq_band_names{i})(1:end),model_fun.(this_model_name),[1.,1.,0  ,0.]);
+            [this_p,R,J,CovB,MSE] =nlinfit(x(1:end),y.(this_band_name)(1:end),model_fun.(this_model_name),[1.,1.,0  ,0.]);
             else
-            [this_p,R,J,CovB,MSE,ErrorModelInfo] =nlinfit(x(1:end),y.(freq_band_names{i})(1:end),model_fun.(this_model_name),[1.,1.,0  ,0.]);
+            [this_p,R,J,CovB,MSE,ErrorModelInfo] =nlinfit(x(1:end),y.(this_band_name)(1:end),model_fun.(this_model_name),[1.,1.,0  ,0.]);
             end
         end
         
-        model_params.(freq_band_names{i}).(this_model_name) = this_p;
-        h=plot(x_temp,model_fun.(this_model_name)( model_params.(freq_band_names{i}).(this_model_name) ,x_temp),colors{i});
+        model_params.(this_band_name).(this_model_name) = this_p;
+        h=plot(x_temp,model_fun.(this_model_name)( model_params.(this_band_name).(this_model_name) ,x_temp),colors{i});
         if this_model_idx==1
             handles = [handles,h];
         end
         
         if MSE<this_bands_mse
             this_bands_mse = MSE ;
-            best_model.(freq_band_names{i}).model_params = this_p;
-            best_model.(freq_band_names{i}).fun = model_fun.(this_model_name);
+            best_model.(this_band_name).model_params = this_p;
+            best_model.(this_band_name).fun = model_fun.(this_model_name);
+            best_model.(this_band_name).model_name = this_model_name ;
         end
         
         
@@ -77,9 +89,9 @@ save('spatial_decay_models_coherence','model_fun','model_params','best_model','b
 %%
 % plot best model
 if true
-    close all
+    %close all
     
-    addpath('../../functionPool/BrewerMap/')
+    addpath('../../../tools/BrewerMap/')
     set(0,'defaultfigurecolor',[1 1 1])
 
     plot_dir = fullfile('/Users/bosse/dev/matlab/mrC/Examples/plots') ;
@@ -108,19 +120,20 @@ if true
     xlabel('Spatial Distance [mm]')
     ylabel('Coherence')
     ylim([0,1])
-    xlim([0,4])
+    xlim([0,5])
     legend(legend_entries,'FontSize',fsz,'location','northeastoutside')
-end
-set(gca, 'FontSize', fsz, 'LineWidth', alw); %<- Set properties
-fig_width = 8.5/2 ;
-fig_height = 5/8*fig_width ;
-fig.PaperPositionMode = 'manual';
-fig.PaperUnits = 'inches';
-fig.Units = 'inches';
-fig.PaperPosition = [0, 0, fig_width,fig_height];
-fig.PaperSize = [fig_width,fig_height];
-fig.Position = [0.0, 0.0, fig_width, fig_height];
-fig.Resize = 'off';
-fig.InvertHardcopy = 'off';
-filename = fullfile(plot_dir,'spatial_decay_coherence');
-print(filename,'-dpdf');
+
+    set(gca, 'FontSize', fsz, 'LineWidth', alw); %<- Set properties
+    fig_width = 8.5/2 ;
+    fig_height = 5/8*fig_width ;
+    fig.PaperPositionMode = 'manual';
+    fig.PaperUnits = 'inches';
+    fig.Units = 'inches';
+    fig.PaperPosition = [0, 0, fig_width,fig_height];
+    fig.PaperSize = [fig_width,fig_height];
+    fig.Position = [0.0, 0.0, fig_width, fig_height];
+    fig.Resize = 'off';
+    fig.InvertHardcopy = 'off';
+    filename = fullfile(plot_dir,'spatial_decay_coherence');
+    print(filename,'-dpdf');
+    end
