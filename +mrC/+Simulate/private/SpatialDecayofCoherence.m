8f17ed9ae408882939393f54276372782b8e1c59
sebobosse <sebastian.bosse@gmail.com>
2019-02-20 10:00:55 +0100

    spatial coherence models base on fig.3 supplementary in kellis et al. 2016

--- +mrC/+Simulate/private/SpatialDecayofCoherence.m
+++ +mrC/+Simulate/private/SpatialDecayofCoherence.m
@@ -9,10 +9,10 @@ clear all
 % model_fun.lorentzian   =  @(p,x)(p(4)+p(1)* ((p(2)^2)./(p(2)^2+(x-p(3)).^2)));
 % model_fun.power_law =  @(p,x)(p(4)+p(1)* (x+p(3)).^(-p(2)));
 
-model_fun.expo = @(p,x)( (1-p(1)) *exp(-p(2)*x) );
-model_fun.lorentzian = @(p,x)( (1-p(1))*(p(2)^2./(p(2)^2+x.^2))    ) ;
-model_fun.gauss = @(p,x) ( (1-p(1)) * exp(-(x.^2) )/p(2)  ) ;
-model_fun.power_law = @(p,x) ( (p(2)*x+1).^-p(1)) ;
+model_fun.expo = @(p,x)( (1) *exp(-p(1)*x) );
+% model_fun.lorentzian = @(p,x)( (1)*(p(1)^2./(p(1)^2+x.^2))    ) ;
+% model_fun.gauss = @(p,x) ( (1) * exp(-(x.^2) )/p(1)  ) ;
+% model_fun.power_law = @(p,x) ( (p(2)*x+1).^-p(1)) ;
 
 % % data taken from Fig. 4
 % x = [0,0.38,0.5,0.54,1:0.5:3] ;% in mm
@@ -22,14 +22,21 @@ model_fun.power_law = @(p,x) ( (p(2)*x+1).^-p(1)) ;
 % y.alpha = [7., 5.1, 4.9, 4.8,  4.45, 4.2,  3.9,  3.8,  3.6]/7;
 % y.gamma = [7., 3.2, 2.9, 2.75, 2.3,  2.05, 1.9,  1.75, 1.6]/7;
 
+% from microEcog3, fig. 3 in supplementary material
+% x =       [0., 0.38, 0.5,   0.54, 1.,   1.5,  2.,   2.5,  3.,   3.5,  4.,   4.5,  5.] ;% in mm
+% y.delta = [1., 0.85, 0.825, 0.81, 0.72, 0.67, 0.62, 0.61, 0.61, 0.61, 0.61, 0.61, 0.61] ;
+% y.theta = [1., 0.9,  0.87,  0.85, 0.8,  0.73, 0.7,  0.67, 0.67, 0.67, 0.67, 0.67, 0.67] ;
+% y.beta =  [1., 0.73, 0.7,   0.68, 0.63, 0.6,  0.55, 0.63, 0.5,  0.48, 0.46, 0.44, 0.4 ];
+% y.alpha = [1., 0.9,  0.87,  0.81, 0.82, 0.79, 0.75, 0.725, 0.71, 0.68, 0.66, 0.62, 0.6 ] ;
+% y.gamma = [1., 0.45, 0.42,  0.39, 0.33, 0.3 , 0.27, 0.25,  0.22, 0.21,  0.2,  0.2, 0.19];
 
-x =       [0., 0.38, 0.5,   0.54, 1.,   1.5,  2.,   2.5,  3.,   3.5,  4.,   4.5,  5.] ;% in mm
-y.delta = [1., 0.85, 0.825, 0.81, 0.72, 0.67, 0.62, 0.61, 0.61, 0.61, 0.61, 0.61, 0.61] ;
-y.theta = [1., 0.9,  0.87,  0.85, 0.8,  0.73, 0.7,  0.67, 0.67, 0.67, 0.67, 0.67, 0.67] ;
-y.beta =  [1., 0.73, 0.7,   0.68, 0.63, 0.6,  0.55, 0.63, 0.5,  0.48, 0.46, 0.44, 0.4 ];
-y.alpha = [1., 0.9,  0.87,  0.81, 0.82, 0.79, 0.75, 0.725, 0.71, 0.68, 0.66, 0.62, 0.6 ] ;
-y.gamma = [1., 0.45, 0.42,  0.39, 0.33, 0.3 , 0.27, 0.25,  0.22, 0.21,  0.2,  0.2, 0.19];
-
+%% data taken from micro ECoG3 in suplimentary material
+x       = [ 0., 0.5,  1.,   1.5,  2.,   2.5,  3.,  3.5,  4.] ;
+y.delta = [ 1., 0.96, 0.94, 0.9,  0.88, 0.84, 0.8, 0.78, 0.76 ];
+y.theta = [ 1., 0.96, 0.94, 0.9,  0.88, 0.84, 0.82, 0.8, 0.78 ];
+y.alpha = [ 1., 0.95, 0.91, 0.86, 0.84, 0.78, 0.72, 0.67, 0.61];
+y.beta  = [ 1., 0.92, 0.84, 0.76, 0.68, 0.61, 0.69, 0.56, 0.48] ;
+y.gamma = [ 1., 0.84, 0.7,  0.68, 0.48, 0.4, 0.36, 0.32, 0.28];
 
 band_freqs.delta = [0,4];
 band_freqs.theta = [4,8];
@@ -37,7 +44,7 @@ band_freqs.alpha = [8,12];
 band_freqs.beta = [12,30];
 band_freqs.gamma = [30,80];
 %
-fig=figure ;
+fig_all_fits=figure ;
 freq_band_names = fieldnames(y) ;
 model_names = fieldnames(model_fun) ;
 
@@ -71,7 +78,7 @@ for i = 1:length(freq_band_names)
         sprintf('band: %s, model: %s, MSE=%f, mean(R)= %f',this_band_name,this_model_name, MSE,mean(R.^2))
         model_params.(this_band_name).(this_model_name) = this_p;
         h=plot(x_temp,model_fun.(this_model_name)( model_params.(this_band_name).(this_model_name) ,x_temp),colors{i});
-        xlim([0,15])
+        xlim([0,5])
         if this_model_idx==1
             handles = [handles,h];
         end
@@ -110,7 +117,7 @@ if true
     lw = 1.5;      % LineWidth
     msz = 8;       % MarkerSize
 
-    x_temp = 0:0.01:5 ;
+    x_temp = 0:0.01:150 ;
     colors = brewermap(length(freq_band_names),'Set2');
     fig = figure;
     legend_entries = {} ;
@@ -124,7 +131,7 @@ if true
     xlabel('Spatial Distance [mm]')
     ylabel('Coherence')
     ylim([0,1])
-    xlim([0,5])
+    xlim([0,70])
     legend(legend_entries,'FontSize',fsz,'location','northeastoutside')
 
     set(gca, 'FontSize', fsz, 'LineWidth', alw); %<- Set properties
@@ -138,6 +145,9 @@ if true
     fig.Position = [0.0, 0.0, fig_width, fig_height];
     fig.Resize = 'off';
     fig.InvertHardcopy = 'off';
-    filename = fullfile(plot_dir,'spatial_decay_coherence');
+    filename = fullfile(plot_dir,'spatial_decay_coherence_expo_only');
+    print(filename,'-dpdf');
+    filename = fullfile(plot_dir,'spatial_decay_coherence_all_fitFuncs_smallRange');
+    figure(fig_all_fits)
     print(filename,'-dpdf');
     end
